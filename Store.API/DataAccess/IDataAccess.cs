57cfb6be5a85b3980f5519f8dd96dd60289312b3
DESKTOP-7HUNCQ5\user <piotr.sviryd@aisnovations.com>
2022-12-02 18:02:38 +0300

    Add project files.

---
+++ Store.API/DataAccess/IDataAccess.cs
@@ -0,0 +1,25 @@
+﻿using Store.API.Models;
+
+namespace Store.API.DataAccess
+{
+    public interface IDataAccess
+    {
+        List<ProductCategory> GetProductCategories();
+        ProductCategory GetProductCategory(int id);
+        Offer GetOffer(int id);
+        List<Product> GetProducts(string category, string subCategory, int count);
+        Product GetProduct(int id);
+        bool InsertUser(User user);
+        string IsUserPresent(string email, string password);  
+        void InsertReview(Review review);
+        List<Review> GetProductReviews(int productId);
+        User GetUser(int id);
+        bool InsertCartItem(int userId, int productId);
+        Cart GetActiveCartOfUser(int userId);   
+        Cart GetCart(int cartId);
+        List<Cart> GetAllPreviousCartOfUser(int userId);
+        List<PaymentMethod> GetPaymentMethod();
+        int InsertPayment(Payment payment);
+        int InsertOrder(Order order); 
+    }
+}
---
+++ Store.API/Models/CartItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Store.API.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+
+        public Product Product { get; set; } = new Product();
+    }
+}
